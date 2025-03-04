6615a4f98ec6f4414eeeaad19b9176f91d14d324
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-03-04 12:53:15 -0600

    Initial Commit

---
+++ CheckPoint8/CheckPoint8App.swift
@@ -0,0 +1,17 @@
+//
+//  CheckPoint8App.swift
+//  CheckPoint8
+//
+//  Created by Benjamin Rojo on 04/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CheckPoint8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheckPoint8/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CheckPoint8
+//
+//  Created by Benjamin Rojo on 04/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
